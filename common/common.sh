7900aa0abde2a26d618d5d4bf458bc3b4f0fe498
Avjit Mondal <avijitmondal38@gmail.com>
2021-06-15 07:49:16 +0530

    Modifed shell script as per IDE suggestion

--- common/common.sh
+++ common/common.sh
@@ -1,10 +1,10 @@
 #this script contains all the common functions need for other script
 
 #Assign date variable
-DATETIME=`date +"%m%d%y_%H%M%S-%Z"`
+DATETIME=$(date +"%m%d%y_%H%M%S-%Z")
 
 # Redirect stdout ( > ) into a named pipe ( >() ) running "tee"
-exec 1> >(tee -ia /tmp/setup_ubuntu_dev_env_$DATETIME.log)
+exec 1> >(tee -ia /tmp/setup_ubuntu_dev_env_"$DATETIME".log)
 
 # Redirect STDERR to STDOUT
 exec 2>&1
@@ -17,22 +17,22 @@ reset_color='\E[00m'
 
 function echo_green {
     message=$1
-    echo -e "${green}`date`: ${message}${reset_color}"
+    echo -e "${green}$(date): ${message}${reset_color}"
 }
 
 function echo_yellow {
     message=$1
-    echo -e "${yellow}`date`: ${message}${reset_color}"
+    echo -e "${yellow}$(date): ${message}${reset_color}"
 }
 
 function echo_red {
     message=$1
-    echo -e "${red}`date`: ${message}${reset_color}"
+    echo -e "${red}$(date): ${message}${reset_color}"
 }
 
 function echo_blue {
     message=$1
-    echo -e "${blue}`date`: ${message}${reset_color}"
+    echo -e "${blue}$(date): ${message}${reset_color}"
 }
 
 function exit_on_error {
@@ -44,7 +44,8 @@ function install_apt_package {
     for package in "$@" 
     do
         echo_yellow "Installing apt package $package"
-        sudo apt-get install -y $package
+        sudo apt-get install -y "$package"
+        # shellcheck disable=SC2181
         if [ $? -ne 0 ];
         then
             exit_on_error "Package $package has not installed properly"
